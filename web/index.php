e7209ce82705756caac4bdbb71f8f66c0db988cf
Carmen Tang <carmentang@fb.com>
2016-07-27 23:23:46 -0700

    Trying to write tagged threat descriptors to a file

--- web/index.php
+++ web/index.php
@@ -34,19 +34,30 @@ $app->get('/get_update.php', function (Silex\Application $app, Symfony\Component
 
 $ent_info = null;
 
+function file_force_contents($dir, $contents){
+    $parts = explode('/', $dir);
+    $file = array_pop($parts);
+    $dir = '';
+    foreach($parts as $part)
+        if(!is_dir($dir .= "/$part")) mkdir($dir);
+    file_put_contents("$dir/$file", $contents);
+}
+
 $app->post('/get_update.php', function (Silex\Application $app, Symfony\Component\HttpFoundation\Request $request) {
   $ent_info = json_decode($request->getContent(), true);
   error_log("\n".print_r(json_decode($request->getContent(), true), true));
+  file_force_contents('threatexchange/tx_info.txt', $ent_info);
   // $app->dumpFile('tx_tag.txt', $request->getContent());
   return 'ok';
 });
 
 // Our web handlers
-$app->post('/', function() use($app) {
+
+$app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  error_log("\n"."@@@@@@@@@@@@@@@@@");
-  error_log("\n\n".print_r($ent_info, true));
-  error_log("\n"."$$$$$$$$$$$$$$$$$$");
+  // error_log("\n"."@@@@@@@@@@@@@@@@@");
+  // error_log("\n\n".print_r($ent_info, true));
+  // error_log("\n"."$$$$$$$$$$$$$$$$$$");
   return $app['twig']->render('index.twig', array('ent_info' => $ent_info,)
   );
 });
