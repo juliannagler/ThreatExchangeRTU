9851b3874b155e0467e50737fa40c94998e67dda
Carmen Tang <carmentang@fb.com>
2016-07-27 23:11:07 -0700

    Trying to write tagged threat descriptors to a file

--- web/index.php
+++ web/index.php
@@ -35,10 +35,7 @@ $app->get('/get_update.php', function (Silex\Application $app, Symfony\Component
 $ent_info = null;
 
 $app->post('/get_update.php', function (Silex\Application $app, Symfony\Component\HttpFoundation\Request $request) {
-  $contents = file_get_contents($request->getContent());
-  $contents = utf8_encode($contents);
-  $ent_info = json_decode($contents, true);
-  $ent_trial = json_decode($contents);
+  $ent_info = json_decode($request->getContent(), true);
   error_log("\n".print_r(json_decode($request->getContent(), true), true));
   // $app->dumpFile('tx_tag.txt', $request->getContent());
   return 'ok';
@@ -49,9 +46,6 @@ $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   error_log("\n"."@@@@@@@@@@@@@@@@@");
   error_log("\n\n".print_r($ent_info, true));
-  error_log("\n\n".print_r(var_dump($ent_info, true)));
-  error_log("\n\n".print_r(var_dump($ent_info, true)));
-  error_log("\n\n".print_r($ent_trial));
   error_log("\n"."$$$$$$$$$$$$$$$$$$");
   return $app['twig']->render('index.twig', array('ent_info' => $ent_info,)
   );
