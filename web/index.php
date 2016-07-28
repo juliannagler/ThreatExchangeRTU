8643e0ee6eaf2acc1f7d34949e6839fd34a58eb5
Carmen Tang <carmentang@fb.com>
2016-07-27 23:12:47 -0700

    Trying to write tagged threat descriptors to a file

--- web/index.php
+++ web/index.php
@@ -42,7 +42,7 @@ $app->post('/get_update.php', function (Silex\Application $app, Symfony\Componen
 });
 
 // Our web handlers
-$app->get('/', function() use($app) {
+$app->get('/get_update.php', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   error_log("\n"."@@@@@@@@@@@@@@@@@");
   error_log("\n\n".print_r($ent_info, true));
